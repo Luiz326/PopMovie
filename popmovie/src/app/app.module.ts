04b8c8fe213cd2eb55a65b67e38592aa51647b2e
Luiz Mitsuru Dai <luizmitsuru@gmail.com>
2021-03-02 13:15:04 -0300

    Organizando pastas

--- popmovie/src/app/app.module.ts
+++ popmovie/src/app/app.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LoginpageComponent } from './loginpage/loginpage.component';
 import { FooterComponent } from './footer/footer.component';
-import { SignuppageComponent } from './signuppage/signuppage.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { Navbar2Component } from './navbar/navbar2/navbar2.component';
-import { MovieselectorpageComponent } from './movieselectorpage/movieselectorpage.component';
-import { DetailsmoviepageComponent } from './detailsmoviepage/detailsmoviepage.component';
-import { FavoritemoviepageComponent } from './favoritemoviepage/favoritemoviepage.component';
+import { LoginpageComponent } from './pages/account/loginpage/loginpage.component';
+import { SignuppageComponent } from './pages/account/signuppage/signuppage.component';
+import { DetailsmoviepageComponent } from './pages/movies/detailsmoviepage/detailsmoviepage.component';
+import { FavoritemoviepageComponent } from './pages/movies/favoritemoviepage/favoritemoviepage.component';
+import { MovieselectorpageComponent } from './pages/movies/movieselectorpage/movieselectorpage.component';
+
 
 @NgModule({
   declarations: [
